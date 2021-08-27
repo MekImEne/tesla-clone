3775639269072cb48d73e47c7ec678edeb5e8834
MekImEne <fi_mekideche@esi.dz>
2021-08-27 01:43:30 +0100

    ADD: Sidebar Menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+//import CloseIcon from "@material-ui/icons/Close";
+
+const menuItems = [
+  { title: "Existing Inventory", href: "/" },
+  { title: "Used Inventory", href: "/" },
+  { title: "Trade-in", href: "/" },
+  { title: "Test Drive", href: "/" },
+  { title: "Cybertruck", href: "/" },
+  { title: "Roadster", href: "/" },
+  { title: "Semi", href: "/" },
+  { title: "Charging", href: "/" },
+  { title: "Powerwall", href: "/" },
+  { title: "Commercial Energy", href: "/" },
+  { title: "Utilities", href: "/" },
+  { title: "Find Us", href: "/" },
+  { title: "Support", href: "/" },
+  { title: "Investor Relations", href: "/" },
+];
 
 function Header() {
+  const [burgerStatus, setBurgerStatus] = useState(false);
   return (
     <Container>
       <a href="/">
@@ -19,8 +38,21 @@ function Header() {
       <RightMenu>
         <a href="/">Shop</a>
         <a href="/">Account</a>
-        <CustomMenu>Menu</CustomMenu>
+        <CustomMenu onClick={() => setBurgerStatus(true)}>Menu</CustomMenu>
       </RightMenu>
+
+      <BurgerNav show={burgerStatus}>
+        <CloseWrapper>
+          {/* <CustomClose /> */}
+          <p onClick={() => setBurgerStatus(false)}>X</p>
+        </CloseWrapper>
+
+        {menuItems.map((item, index) => (
+          <li key={index}>
+            <a href={item.href}> {item.title} </a>
+          </li>
+        ))}
+      </BurgerNav>
     </Container>
   );
 }
@@ -37,6 +69,7 @@ const Container = styled.div`
   top: 0;
   left: 0;
   right: 0;
+  z-index: 1;
 `;
 
 const Menu = styled.div`
@@ -71,3 +104,40 @@ const RightMenu = styled.div`
 const CustomMenu = styled.a`
   cursor: pointer;
 `;
+
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background-color: white;
+  width: 300px;
+  z-index: 100;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  transform: ${(props) => (props.show ? "translateX(0)" : "translateX(100%)")};
+  transition: transform 0.5s;
+
+  li {
+    padding: 15px;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+
+    a {
+      font-weight: 600;
+    }
+  }
+`;
+
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+
+  p {
+    cursor: pointer;
+  }
+`;
+
+// const CustomClose = styled(CloseIcon)``;
